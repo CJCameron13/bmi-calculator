605595907d9dbbac9dfdf992de469eef78661815
CJ Cameron <96845401+CJCameron13@users.noreply.github.com>
2023-09-07 18:52:55 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ const calculatorContainer = document.querySelector('#calculator-container')
 const metricBoxes = document.querySelector('#user-entry-metric')
 const metricSelector = document.querySelector('#metric')
 const imperialSelector = document.querySelector('#imperial')
-console.log(metricSelector)
+const blueContainer = document.querySelector('#blue-container')
 
 metricSelector.addEventListener('click', () => {
     console.log('item clicked')
     calculatorContainer.classList.add('metric')
     calculatorContainer.classList.remove('imperial')
+    blueContainer.classList.remove('welcome')
+    blueContainer.classList.add('results')
 })
 
 imperialSelector.addEventListener('click', () => {
     calculatorContainer.classList.add('imperial')
     calculatorContainer.classList.remove('metric')
+    blueContainer.classList.remove('welcome')
+    blueContainer.classList.add('results')
 })
